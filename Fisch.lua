a8a5a20c98588c4574a12981a496e7f5cf34ac6a
Quirlynxxx <kirymewwoffc@gmail.com>
2024-11-20 19:24:41 +0200

    Update Fisch.lua

--- Fisch.lua
+++ Fisch.lua
@@ -3,7 +3,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 --Create Main Window
 local Window = Rayfield:CreateWindow({
-   Name = "[🍄] Fisch | Version 0.0.55_fix5",
+   Name = "[🍄] Fisch | Version 0.0.55_fix4",
    LoadingTitle = "[🍄] Fisch",
    LoadingSubtitle = "by Kirymeww",
    Theme = "Default",
